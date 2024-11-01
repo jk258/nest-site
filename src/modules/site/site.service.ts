75795e3ae45175729a62f4fc83c6f6e6b9dd026c
renguowei <18135185792@163.com>
2024-11-01 18:25:19 +0800

    feat: 图片保存

--- src/modules/site/site.service.ts
+++ src/modules/site/site.service.ts
@@ -5,12 +5,16 @@ import { HttpService } from '@nestjs/axios'
 import * as cheerio from 'cheerio'
 import { ResUserDto } from '@/modules/user/dto/user.dto'
 import { UserRole } from '@/types/enmus'
+import { writeFileSync } from 'fs'
+import { join } from 'path'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class SiteService {
 	constructor(
 		private readonly prisma: PrismaService,
-		private readonly httpService: HttpService,
+    private readonly httpService: HttpService,
+    private readonly config: ConfigService
 	) {}
 	async create(user: ResUserDto, createSiteDto: CreateSiteDto) {
     try {
@@ -29,7 +33,8 @@ export class SiteService {
 			})
 			if (site) {
 				throw new BadRequestException(createSiteDto.title + '已存在')
-			} else {
+      } else {
+        createSiteDto.logo = createSiteDto.logo.replace(this.config.get<string>('SITE_DOMAIN'), '')
 				return await this.prisma.site.create({
 					data: {
 						...createSiteDto,
@@ -62,7 +67,8 @@ export class SiteService {
 			})
 			return siteList.map((site) => {
 				return {
-					...site,
+          ...site,
+          logo:site.logo?site.logo:this.config.get<string>('SITE_DOMAIN')+site.logo,
 					tags: site.tags.map((tag) => tag.tag),
 				}
 			})
@@ -107,7 +113,8 @@ export class SiteService {
 				return {
 					tagId: Number(tag),
 				}
-			})
+      })
+      siteDto.logo = siteDto.logo.replace(this.config.get<string>('SITE_DOMAIN'), '')
 			return await this.prisma.site.update({
 				where: {
 					id: siteDto.id,
@@ -164,15 +171,30 @@ export class SiteService {
 	}
 
 	async getSiteInfo(url: string) {
-		try {
-			const res = await this.httpService.axiosRef.get(url)
+    try {
+      const res = await this.httpService.axiosRef.get(url, {
+        headers: {
+          "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
+        }
+      })
 			const $ = cheerio.load(res.data)
 			const title = $('title').text()
 			const desc = $('meta[name="description"]').attr('content')
-			let logo = $('link[rel="shortcut icon"]').attr('href') || $('link[rel="icon"]').attr('href')
-			if (logo && !this.isValidUrl(logo)) {
-				logo = url + logo
+      let logo = $('link[rel="shortcut icon"]').attr('href') || $('link[rel="icon"]').attr('href')
+			if (logo) {
+				if (!this.isValidUrl(logo)) {
+					logo = url + logo
+				}
+				const resLogo = await this.httpService.axiosRef.get(logo, {
+					responseType: 'arraybuffer',
+        })
+        const buffer = Buffer.from(resLogo.data)
+        const logoPaths = logo.split('/')
+        const logoName = String(+new Date()) + '-' + String(Math.random()).split('.')[1] + '-' + logoPaths[logoPaths.length - 1]
+        writeFileSync(join(__dirname, '../../../static/upload/' + logoName), buffer)
+        logo = this.config.get<string>('SITE_DOMAIN')+'/upload/' + logoName
 			}
+      
 			return { title, desc, logo }
 		} catch (error) {
 			throw new BadRequestException(error)
